a0acfd823479bc919b326b7de18e2ba97b3282a8
Nazariman <NazarBor.iii@gmail.com>
2025-04-10 23:30:45 +0300

    Add task2-3

--- HW2/task2-3/client2.py
+++ HW2/task2-3/client2.py
@@ -1,5 +1,15 @@
 # ===== КЛІЄНТ (client.py) =====
 import socket
+import threading
+
+def receive_message(client):
+    while True:
+        try:
+            msg = client.recv(1024).decode('utf-8')
+            if msg:
+                print("\n" + msg)
+        except:
+            break
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -10,6 +20,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.send(name.encode('utf-8'))
 
     print(s.recv(1024).decode('utf-8')) # повідомлення з іменем співрозмовника
+
+    t = threading.Thread(target=receive_message, args=(s,), daemon=True)
+    t.start()
 
     while True:
         msg = input("Ви: ")
--- HW2/task2-3/server.py
+++ HW2/task2-3/server.py
@@ -1,8 +1,19 @@
 # ===== СЕРВЕР (server.py) =====
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 65432
+
+def send_message(from_client, to_client, from_name):
+    while True:
+        try:
+            msg = from_client.recv(1024).decode('utf-8')
+            if msg.lower() == 'вихід':
+                break
+            to_client.send(f"{from_name}: {msg}".encode('utf-8'))
+        except:
+            break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     s.bind((HOST, PORT))
@@ -20,18 +31,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn1.send(f"Спілкуєтесь з {name2}".encode('utf-8'))
     conn2.send(f"Спілкуєтесь з {name1}".encode('utf-8'))
 
-    try:
-        while True:
-            msg1 = conn1.recv(1024).decode('utf-8')
-            if msg1.lower() == 'вихід':
-                break
-            conn2.send(f"{name1}: {msg1}".encode('utf-8'))
+    t1 = threading.Thread(target=send_message, args=(conn1, conn2, name1))
+    t2 = threading.Thread(target=send_message, args=(conn2, conn1, name2))
+    t1.start()
+    t2.start()
 
-            msg2 = conn2.recv(1024).decode('utf-8')
-            if msg2.lower() == 'вихід':
-                break
-            conn1.send(f"{name2}: {msg2}".encode('utf-8'))
-    finally:
-        conn1.close()
-        conn2.close()
-        print("[Сервер] З'єднання закрито")
+    t1.join()
+    t2.join()
+
+    conn1.close()
+    conn2.close()
+    print("[Сервер] З'єднання закрито")
